73d0ea502cd998655875f6434115d09e8e868e6e
nvalt-1 <nvaltierra@protonmail.com>
2021-11-17 09:03:23 -0600

    Server response now conforms to new format
    Changed how the server responds to allow for future expansions and to
    stay consistent with other responses.

--- routes/employees.js
+++ routes/employees.js
@@ -7,27 +7,44 @@ module.exports = router;
 
 // Get all employees currently in database
 router.get('/employees', async (req, res) => {
-  const args = ['employee', 'LIMIT 100'];
-  const dbQuery = format.withArray('SELECT *\nFROM %I\n%s;\n', args);
+  const args = ['employee', 'id' ,'LIMIT 100'];
+  const dbQuery = format.withArray('SELECT *\nFROM %I\nORDER BY %I\n%s;', args);
   try {
     const result = await db.query(dbQuery);
-    res.json(result.rows);
+    res.json({
+      rows: result.rows, 
+      queries: [dbQuery],
+      transaction: false
+    });
   } 
   catch(err) {
     console.error(err.stack);
-    res.json({error: 'invalid query'});
+    res.json({
+      error: 'invalid query',
+      queries: [dbQuery],
+      transaction: false
+    });
   }
 });
 
 // Insert a new employee into the ElephantSQL employee database
 router.post('/employees', async (req, res) => {
   const {id, first_name, last_name, job_title, salary} = req.body;
-  const dbQuery = format('INSERT INTO %I VALUES($1, $2, $3, $4, $5)\nRETURNING *;\n', 'employee');
+  const args = ['employee', id, first_name, last_name, job_title, salary];
+  const dbQuery = format.withArray('INSERT INTO %I VALUES(%L, %L, %L, %L, %L)\nRETURNING *;', args);
   try {
-    const result = await db.query(dbQuery, [id, first_name, last_name, job_title, salary]);
-    res.json(result.rows[0]);
+    const result = await db.query(dbQuery);
+    res.json({
+      rows: result.rows, 
+      queries: [dbQuery],
+      transaction: false
+    });
   } catch(err) {
     console.error(err.stack);
-    res.json({error: 'invalid query'});
+    res.json({
+      error: 'invalid query',
+      queries: [dbQuery],
+      transaction: false
+    });
   }
 });
