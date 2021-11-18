36c68793cf63c43a34b39004a3e34dffd241ac1e
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-18 16:28:06 -0600

    Fixed leftover mistakes from initial commit

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require("cors");
+const cors = require('cors');
 const mountRoutes = require('./routes');
 require('dotenv').config();
 
@@ -9,7 +9,7 @@ const server = app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
 
-app.use(express.static('public'));
+// app.use(express.static('client'));
 app.use(cors());
 app.use(express.json());
 
