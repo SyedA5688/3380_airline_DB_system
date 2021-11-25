77752962cf9ed4523b4f66511462d43ce914c5d3
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-25 10:52:30 -0600

    Fixed bug. Added GET /employee/:id/leave

--- routes/employee-id.js
+++ routes/employee-id.js
@@ -38,16 +38,16 @@ module.exports = router;
 router.get('/employee/:id', async (req, res) => {
   const id = req.params.id;
   if(id && /^\d+$/.test(id)) {
+    const joinArgs = [
+      'employee',
+      'NATURAL JOIN employee_address',
+      'NATURAL JOIN job',
+      'NATURAL JOIN department',
+      'NATURAL JOIN benefits',
+      'NATURAL JOIN salary'
+    ];
+    const dbQuery = format('SELECT *\nFROM %I e\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s\nWHERE %I = %L;', ...joinArgs, 'employee_id', id);
     try {
-      const joinArgs = [
-        'employee',
-        'NATURAL JOIN employee_address',
-        'NATURAL JOIN job',
-        'NATURAL JOIN department',
-        'NATURAL JOIN benefits',
-        'NATURAL JOIN salary',
-      ];
-      const dbQuery = format('SELECT *\nFROM %I e\n\t%s\n\t%s\n\t%s\n\t%s\n\t%s;', ...joinArgs);
       const result = await db.query(dbQuery); 
       res.json({
         rows: result.rows, 
@@ -58,10 +58,107 @@ router.get('/employee/:id', async (req, res) => {
       console.error(err.stack);
       res.status(422).json({
         error: err.message,
-        queries: [],
+        queries: [dbQuery],
         transaction: false
       });
     }
+  } else {
+    res.status(400).json({
+      error: 'Invalid or missing id',
+      queries: [],
+      transaction: false
+    });
+  }
+});
+
+/**
+ * @api {get} /employee/:id Get employee leave details
+ * @apiName GetEmployeeLeave
+ * @apiGroup Employee
+ * @apiDescription Returns the employee with the given id's leave details
+ * 
+ * @apiParam {Number}     id  Employee's ID number
+ * 
+ * @apiSuccess {Object[]} rows                  Results from the database. Returns information from leave table
+ * @apiSuccess {String[]} queries               Array of queries used
+ * @apiSuccess {Boolean}  transaction           True if transactions were used
+ * 
+ * @apiSuccessExample {json} Success-Response example:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "rows": [{
+ *                "leave_id": "123",
+ *                "employee_id": "1000000",
+ *                "date": "2010-05-05",
+ *                "reason": "MEDICAL",
+ *                "status": "APPROVED"
+ *               }],
+ *      "queries": ["SELECT *\nFROM table;"],
+ *      "transaction": false
+ *    }
+ * 
+ */
+router.get('/employee/:id/leave', async (req, res) => {
+  const id = req.params.id;
+  if(id && /^\d+$/.test(id)) {
+    const args = [
+      'leave',
+      'employee_id',
+      id
+    ];
+    const dbQuery = format('SELECT *\nFROM %I\nWHERE %I = %L;', ...args);
+    try {
+      const result = await db.query(dbQuery); 
+      res.json({
+        rows: result.rows, 
+        queries: [dbQuery],
+        transaction: false
+      });
+    } catch {
+      console.error(err.stack);
+      res.status(422).json({
+        error: err.message,
+        queries: [dbQuery],
+        transaction: false
+      });
+    }
+  } else {
+    res.status(400).json({
+      error: 'Invalid or missing id',
+      queries: [],
+      transaction: false
+    });
+  }
+});
+
+/**
+ * @api {post} /employee/:id Change employee details*
+ * @apiName ChangeEmployeeDetails
+ * @apiGroup Employee
+ * @apiDescription Attempts to alter employee details based on body parameters. *Not yet implemented
+ * 
+ * @apiParam {Number}     id  Employee's ID number
+ * 
+ * 
+ * @apiSuccess {Object[]} rows                  Results from the database. Returns employee ID.
+ * @apiSuccess {String[]} queries               Array of queries used
+ * @apiSuccess {Boolean}  transaction           True if transactions were used
+ * 
+ * @apiSuccessExample {json} Success-Response example:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "rows": [{
+ *                "employee_id": 1000000
+ *               }],
+ *      "queries": ["UPDATE table\nSET column\nWHERE employee_id = 1000000;"],
+ *      "transaction": true
+ *    }
+ * 
+ */
+router.post('/employee/:id', async (req, res) => {
+  const id = req.params.id;
+  if(id && /^\d+$/.test(id)) {
+
   } else {
     res.status(400).json({
       error: 'Invalid or missing id',
