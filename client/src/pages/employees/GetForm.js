4a74abdbe63e7147b04441f7fb218a27f8dab43c
Syed Rizvi <asad5688@gmail.com>
2021-11-27 16:07:15 -0600

    revised and improved get employee details form, now have option of what details user wishes to see for returned employee

--- client/src/pages/employees/GetForm.js
+++ client/src/pages/employees/GetForm.js
@@ -6,8 +6,58 @@ class GetForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      getID: ''
+      getID: '',
+      showFirstName: false,
+      showMInitial: false,
+      showLastName: false,
+      showID: false,
+      showDOB: false,
+      showSSN: false,
+      showGender: false,
+      showEmail: false,
+      showPhone: false,
+      showStreetAddress: false,
+      showCity: false,
+      showState: false,
+      showZipCode: false,
+      showCountry: false,
+      showJobTitle: false,
+      showDepartmentName: false,
+      showWeeklyHours: false,
+      showHourlyWage: false,
+      showAnnualBonus: false,
+      showBenefitsAmount: false,
+      showStockOptions: false,
+      showHealthInsuranceProvider: false,
+      showRetirementPlanNumber: false
     };
+    
+    // Bad coupling if DB response changes names/details, but need to control which response attributes are shown
+    this.db_to_state = {
+      "first_name": "showFirstName",
+      "m_initial": "showMInitial",
+      "last_name": "showLastName",
+      "employee_id": "showID",
+      "dob": "showDOB",
+      "ssn": "showSSN",
+      "gender": "showGender",
+      "email": "showEmail",
+      "phone": "showPhone",
+      "street_address": "showStreetAddress",
+      "city": "showCity",
+      "state": "showState",
+      "zip_code": "showZipCode",
+      "country": "showCountry",
+      "job_title": "showJobTitle",
+      "department_name": "showDepartmentName",
+      "weekly_hours": "showWeeklyHours",
+      "hourly_wage": "showHourlyWage",
+      "annual_bonus": "showAnnualBonus",
+      "amount": "showBenefitsAmount",
+      "stock_options": "showStockOptions",
+      "health_insurance_provider": "showHealthInsuranceProvider",
+      "retirement_plan": "showRetirementPlanNumber"
+    }
     
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -52,7 +102,7 @@ class GetForm extends Component {
       const responseBody = await response.json();
 
       this.assertValidDBResponse(responseBody);
-      console.log(responseBody);
+      // console.log(responseBody.rows);
 
       this.setState({
         returnedEmployee: responseBody.rows
@@ -80,6 +130,12 @@ class GetForm extends Component {
     const form = document.querySelector('#getFormHTML');
     form.classList.remove('was-validated');
   }
+
+  handleCheckboxChange = async (event) => {
+    this.setState({
+      [event.target.name]: event.target.checked
+    });
+  };
   
   render() {
     return (
@@ -100,14 +156,129 @@ class GetForm extends Component {
           </div>
         </div>
 
-        <h3>Get an Employee's Details</h3>
+        <h3 className="mt-3" >Get an Employee's Details</h3>
 
         <form className="border border-secondary mt-3 px-5 py-4 rounded needs-validation" id="getFormHTML" onSubmit={this.handleSubmit} noValidate>
           
-          <div className="form-group">
+          <div className="form-group mb-3">
             <label className="sr-only" htmlFor="getEmployeeID">Employee ID</label>
             <input type="number" step="1" pattern="[0-9]+" className="form-control" id="getEmployeeID" value={this.state.getID} name="getID" onChange={this.handleChange} required />
             <div className="invalid-feedback">Please input a valid Employee ID.</div>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox1" name="showFirstName" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox1">First Name</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox2" name="showMInitial" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox2">Middle Initial</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox3" name="showLastName" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox3">Last Name</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox4" name="showID" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox4">Employee ID</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox5" name="showDOB" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox5">Date of Birth</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox6" name="showSSN" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox6">Social Security Number</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox7" name="showGender" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox7">Gender</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox8" name="showEmail" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox8">Email</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox10" name="showPhone" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox10">Phone</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox11" name="showStreetAddress" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox11">Street Address</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox12" name="showCity" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox12">City</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox13" name="showState" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox13">State</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox14" name="showZipCode" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox14">Zip Code</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox15" name="showCountry" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox15">Country</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox16" name="showJobTitle" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox16">Job Title</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox17" name="showDepartmentName" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox17">Department Name</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox18" name="showWeeklyHours" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox18">Weekly Hours</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox19" name="showHourlyWage" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox19">Hourly Wage</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox20" name="showAnnualBonus" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox20">Annual Bonus</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox21" name="showBenefitsAmount" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox21">Benefits Amount</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox22" name="showStockOptions" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox22">Stock Options</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox23" name="showHealthInsuranceProvider" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox23">Health Insurance Provider</label>
+          </div>
+
+          <div className="custom-control custom-checkbox">
+            <input type="checkbox" className="custom-control-input" id="checkbox24" name="showRetirementPlanNumber" onChange={this.handleCheckboxChange} />
+            <label className="custom-control-label mx-2" htmlFor="checkbox24">Retirement Plan Number</label>
           </div>
 
           <button type="submit" className="btn btn-outline-secondary mt-3">Submit</button>
@@ -120,25 +291,24 @@ class GetForm extends Component {
           <table align="center" className="table mt-5 border" >
             <thead className="table-dark">
               <tr>
-                <th scope="col">Employee ID</th>
-                <th scope="col">Employee Address</th>
-                <th scope="col">Job</th>
-                <th scope="col">Department</th>
-                <th scope="col">Benefits</th>
-                <th scope="col">Salary</th>
+                <th scope="col">Employee Attribute</th>
+                <th scope="col">Returned Value</th>
               </tr>
             </thead>
             <tbody>
-              {this.state.returnedEmployee && this.state.returnedEmployee.map(employeeObj => (
-                <tr key={employeeObj.employee_id}>
-                  <th scope="col">{employeeObj.first_name}</th>
-                  <th scope="col">{employeeObj.last_name}</th>
-                  <th scope="col">{employeeObj.m_initial}</th>
-                  <th scope="col">{employeeObj.employee_id}</th>
-                  <th scope="col">{employeeObj.job_title}</th>
-                  <th scope="col">{employeeObj.department_name}</th>
-                </tr>
-              ))}
+              {this.state.returnedEmployee && Object.keys(this.state.returnedEmployee[0]).map(responseKey => {
+                let elem = null;
+                if (this.state[this.db_to_state[responseKey]]) {
+                  elem = (<tr key={responseKey + "_row_key"}>
+                    <th scope="col">{responseKey}</th>
+                    <th scope="col">{this.state.returnedEmployee[0][responseKey]}</th>
+                  </tr>)
+                }
+                else {
+                  elem = null
+                }
+                return elem
+              })}
             </tbody>
           </table>
         </div>:
