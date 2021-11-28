df173f64312eecc5a77f17131e45d96bb3083674
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-28 14:08:39 -0600

    Added POST /payroll and changed script

--- routes/payroll.js
+++ routes/payroll.js
@@ -12,18 +12,19 @@ module.exports = router;
  * @apiGroup Payroll
  * @apiDescription Returns all payroll entries.
 
- * @apiQuery {String {non-empty}} [q]             Filter rows based on query
- * @apiQuery {String=id,period}   [searchBy=id]   Used if <code>q</code> is specified.
- * @apiQuery {Number {1+}}        [page=1]        Specify <code>page</code> if there are more results than <code>limit</code>
- * @apiQuery {String=id,period}   [sort=id]       How rows are sorted
- * @apiQuery {String=asc,desc}    [order=asc]     Receive rows in ascending or descending order
- * @apiQuery {Number {1-100}}     [limit=10]      The maximum number of rows to receive
+ * @apiQuery {String {non-empty}}             [q]                 Filter rows based on query
+ * @apiQuery {String=id,period,employee,job}  [searchBy=period]   Used if <code>q</code> is specified. <code>employee</code> and <code>job</code> searches by ID
+ * @apiQuery {Number {1+}}                    [page=1]            Specify <code>page</code> if there are more results than <code>limit</code>
+ * @apiQuery {String=id,period,employee,job}  [sort=period]       How rows are sorted
+ * @apiQuery {String=asc,desc}                [order=asc]         Receive rows in ascending or descending order
+ * @apiQuery {Number {1-100}}                 [limit=10]          The maximum number of rows to receive
  * 
  * @apiSuccess {Object[]} rows                  Results from the database
  * @apiSuccess {Number}   rows.payroll_id       Payroll ID
  * @apiSuccess {Number}   rows.employee_id      Employee ID
+ * @apiSuccess {Number}   rows.job_id           Job ID
  * @apiSuccess {Number}   rows.salary_id        Salary ID
- * @apiSuccess {Number}   rows.leave_id         Leave ID
+ * @apiSuccess {Number}   [rows.leave_id]       Leave ID
  * @apiSuccess {Number}   rows.hours_worked     Hours worked by employee
  * @apiSuccess {String}   rows.pay_period       Pay period
  * @apiSuccess {Number}   rows.tax_rate         Tax rate
@@ -37,6 +38,7 @@ module.exports = router;
  *      "rows": [{
  *                "payroll_id": 123,
  *                "employee_id": 1000000,
+ *                "job_id": 555,
  *                "salary_id": 321,
  *                "leave_id": 100,
  *                "hours_worked": 42,
@@ -53,7 +55,9 @@ router.get('/payroll', async (req, res) => {
   const page = params.page ? params.page : 1;
   const sortParams = {
     id: 'payroll_id',
-    period: 'pay_period'
+    period: 'pay_period',
+    employee: 'employee_id',
+    job: 'job_id'
   };
   const sortBy = sortParams[params.sort] ? sortParams[params.sort] : sortParams.id;
   const order = params.order ? params.order.toUpperCase() : 'ASC';
@@ -92,9 +96,122 @@ router.get('/payroll', async (req, res) => {
   }
 });
 
+/**
+ * @api {post} /payroll Create a new payroll entry
+ * @apiName CreatePayroll
+ * @apiGroup Payroll
+ * @apiDescription Attempts to insert a new payroll entry into the database. Returns the inserted entry.
+ * 
+ * @apiBody {Number}   employee_id      Employee ID
+ * @apiBody {Number}   hours_worked     Hours worked by employee
+ * @apiBody {String}   pay_period       Pay period
+ * @apiBody {Number}   tax_rate         Tax rate
+ * @apiBody {Number}   [job_id]         Job ID. If left blank, will default to current job
+ * @apiBody {Number}   [salary_id]      Salary ID. If left blank, will default to current salary.
+ * @apiBody {Number}   [leave_id]       Leave ID. Won't actually affect anything for the sake of simplicity.
+ * 
+ * @apiSuccess {Object[]} rows                  Results from the database
+ * @apiSuccess {Number}   rows.payroll_id       Payroll ID
+ * @apiSuccess {Number}   rows.employee_id      Employee ID
+ * @apiSuccess {Number}   rows.salary_id        Salary ID
+ * @apiSuccess {Number}   [rows.leave_id]       Leave ID
+ * @apiSuccess {Number}   rows.hours_worked     Hours worked by employee
+ * @apiSuccess {String}   rows.pay_period       Pay period
+ * @apiSuccess {Number}   rows.tax_rate         Tax rate
+ * 
+ * @apiSuccess {String[]} queries               Array of queries used
+ * @apiSuccess {Boolean}  transaction           True if transactions were used
+ * 
+ * @apiSuccessExample {json} Success-Response example:
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "rows": [{
+ *                "payroll_id": 123,
+ *                "employee_id": 1000000,
+ *                "salary_id": 321,
+ *                "leave_id": null,
+ *                "hours_worked": 42,
+ *                "pay_period": "2021-11-01"
+ *                "tax_rate": 0.10
+ *               }],
+ *      "queries": ["SELECT *\nFROM table;"],
+ *      "transaction": false
+ *    }
+ * 
+ */
 router.post('/payroll', async (req, res) => {
+  // TODO: input validation
+  const body = req.body;
+  // Check required fields exist
+  const requiredFields = ['employee_id', 'hours_worked', 'pay_period', 'tax_rate'];
+  const optionalFields = ['job_id', 'salary_id', 'leave_id'];
+  if(utils.checkRequiredFields(requiredFields, body)) {
+    const client = await db.connect().catch((err) => {
+      console.log(err.stack);
+      res.status(422).json({
+        error: 'Error connecting to database',
+        queries: [],
+        transaction: false
+      });
+    });
+    if(client) {
+      let queries = [];
+      try {
+        await utils.transacQuery(queries, client, 'BEGIN TRANSACTION;');
+        await utils.transacQuery(queries, client, 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;');
 
+        // Check employee exists and get optional ids
+        const params = utils.getParameters(requiredFields, optionalFields, body);
+        const args = [
+          optionalFields, 
+          'employee', 
+          'salary', 
+          'leave', 
+          'employee_id',
+          body.employee_id
+        ];
+        let dbQuery = format('SELECT %I\nFROM %I e\n\tNATURAL JOIN %I\n\tLEFT JOIN %I l\n\tON l.%I = e.%5$I\nWHERE e.%5$I = %L;', ...args);
+        const ids = (await utils.transacQuery(queries, client, dbQuery)).rows;
+        if(!ids.length) throw new Error('Invalid employee');
+        optionalFields.forEach((field) => {
+          if(!params.names.includes(field) && ids[0][field] && field !== 'leave_id') {
+            params.names.push(field);
+            params.values.push(ids[0][field]);
+          }
+        });
+
+        dbQuery = format('INSERT INTO %I (%I)\nVALUES (%L)\nRETURNING *;', 'payroll', params.names, params.values);
+        const result = await utils.transacQuery(queries, client, dbQuery);
+
+        await utils.transacQuery(queries, client, 'COMMIT;');
+        await utils.transacQuery(queries, client, 'END TRANSACTION;\n');
+        client.release();
+        res.json({
+          rows: result.rows,
+          queries: queries,
+          transaction: true
+        });
+      } catch(err) {
+        console.log(err.stack);
+        await utils.transacQuery(queries, client, 'ROLLBACK;\n');
+        client.release();
+        res.status(422).json({
+          error: err.message,
+          queries: queries,
+          transaction: true
+        });
+      }
+    }
+  } else {
+    res.status(422).json({
+      error: 'Missing required fields',
+      requiredFields: requiredFields,
+      queries: [],
+      transaction: false
+    });
+  }
 });
+
 
 router.get('/payroll/:id', async (req, res) => {
 
