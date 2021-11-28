3bdff675bc8c3d9ec6fbfe1f8dec5d22b5914597
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-27 20:07:14 -0600

    Created GET /manager

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ const employee = require('./employee');
 const job = require('./job');
 const benefits = require('./benefits');
 const department = require('./department');
+const manager = require('./manager');
 
 module.exports = (app) => {
   app.use(employee);
   app.use(job);
   app.use(benefits);
   app.use(department);
+  app.use(manager);
 };
