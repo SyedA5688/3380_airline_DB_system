5fc585b483b819928791542fed28167fc0f1c0ae
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-28 18:21:56 -0600

    Job location endpoints

--- routes/job/index.js
+++ routes/job/index.js
@@ -2,9 +2,11 @@ const Router = require('express-promise-router');
 
 const job = require('./job');
 const jobID = require('./job-id');
+const jobLocation = require('./job-location');
 
 const router = new Router();
 module.exports = router;
 
 router.use(job);
 router.use(jobID);
+router.use(jobLocation);
