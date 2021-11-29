25d32c6b1e9a7e1a2dd8361434385ac28c90b6ec
nvalt-1 <nvaltierra@protonmail.com>
2021-11-29 10:23:01 -0600

    Finished POST /payroll

--- routes/payroll.js
+++ routes/payroll.js
@@ -108,18 +108,16 @@ router.get('/payroll', async (req, res) => {
  * @apiBody {Number}   hours_worked     Hours worked by employee
  * @apiBody {String}   pay_period       Pay period
  * @apiBody {Number}   tax_rate         Tax rate
- * @apiBody {Number}   [job_id]         Job ID. If left blank, will default to current job
- * @apiBody {Number}   [salary_id]      Salary ID. If left blank, will default to current salary.
- * @apiBody {Number}   [leave_id]       Leave ID. Won't actually affect anything for the sake of simplicity.
  * 
- * @apiSuccess {Object[]} rows                  Results from the database
- * @apiSuccess {Number}   rows.payroll_id       Payroll ID
- * @apiSuccess {Number}   rows.employee_id      Employee ID
- * @apiSuccess {Number}   rows.salary_id        Salary ID
- * @apiSuccess {Number}   [rows.leave_id]       Leave ID
- * @apiSuccess {Number}   rows.hours_worked     Hours worked by employee
- * @apiSuccess {String}   rows.pay_period       Pay period
- * @apiSuccess {Number}   rows.tax_rate         Tax rate
+ * @apiSuccess {Object[]} rows                Results from the database
+ * @apiSuccess {Number}   rows.payroll_id     Payroll ID
+ * @apiSuccess {Number}   rows.employee_id    Employee ID
+ * @apiSuccess {Number}   rows.hours_worked   Hours worked by employee
+ * @apiSuccess {String}   rows.pay_period     Pay period
+ * @apiSuccess {Number}   rows.tax_rate       Tax rate
+ * @apiSuccess {String}   rows.gross_income   Gross income
+ * @apiSuccess {String}   rows.taxed_income   Taxed income
+ * @apiSuccess {String}   rows.net_income     Net income
  * 
  * @apiSuccess {String[]} queries               Array of queries used
  * @apiSuccess {Boolean}  transaction           True if transactions were used
@@ -130,11 +128,12 @@ router.get('/payroll', async (req, res) => {
  *      "rows": [{
  *                "payroll_id": 123,
  *                "employee_id": 1000000,
- *                "salary_id": 321,
- *                "leave_id": null,
  *                "hours_worked": 42,
  *                "pay_period": "2021-11-01"
- *                "tax_rate": 0.10
+ *                "tax_rate": 0.10,
+ *                "gross_income": "$3,200.00",
+ *                "taxed_income": "$320.00",
+ *                "net_income": "$2880.00",
  *               }],
  *      "queries": ["INSERT INTO table VALUES(...);"],
  *      "transaction": true
@@ -146,7 +145,6 @@ router.post('/payroll', async (req, res) => {
   const body = req.body;
   // Check required fields exist
   const requiredFields = ['employee_id', 'hours_worked', 'pay_period', 'tax_rate'];
-  const optionalFields = ['job_id', 'salary_id', 'leave_id'];
   if(utils.checkRequiredFields(requiredFields, body)) {
     const client = await db.connect().catch((err) => {
       console.log(err.stack);
@@ -162,213 +160,54 @@ router.post('/payroll', async (req, res) => {
         await utils.transacQuery(queries, client, 'BEGIN TRANSACTION;');
         await utils.transacQuery(queries, client, 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;');
 
-        // Check employee exists and get optional ids
-        const params = utils.getParameters(requiredFields, optionalFields, body);
-        const args = [
-          optionalFields, 
-          'employee', 
-          'salary', 
-          'leave', 
+        let args = [
+          format('%L::INT', body.hours_worked),
+          'weekly_hours',
+          'hourly_wage'
+        ];
+        const calcGross = format('\n\t\tCASE\n\t\t\tWHEN %s <= 4.4 * %I THEN %1$s * %3$I\n\t\t\tELSE (1.5 * %1$s - 0.5* 4.4 * %I) * %I\n\t\tEND gross_income', ...args);
+        
+        args = [
+          'employee_id',
+          format('%L::INT', body.hours_worked),
+          format('%L::DATE', body.pay_period),
+          format('%L::REAL', body.tax_rate)
+        ];
+        const calcGrossColumns = format('e.%I, %s AS hours_worked, %s AS pay_period, %s AS tax_rate', ...args);
+        
+        args = [
+          calcGrossColumns,
+          calcGross,
+          'employee',
+          'job',
+          'job_id',
+          'salary',
           'employee_id',
           body.employee_id
         ];
-        let dbQuery = format('SELECT %I\nFROM %I e\n\tNATURAL JOIN %I\n\tLEFT JOIN %I l\n\tON l.%I = e.%5$I\nWHERE e.%5$I = %L;', ...args);
-        const ids = (await utils.transacQuery(queries, client, dbQuery)).rows;
-        if(!ids.length) throw new Error('Invalid employee');
-        optionalFields.forEach((field) => {
-          if(!params.names.includes(field) && ids[0][field] && field !== 'leave_id') {
-            params.names.push(field);
-            params.values.push(ids[0][field]);
-          }
-        });
+        const calcGrossString = '\n\tSELECT %s,%s\n\tFROM %I e\n\t\tJOIN %I j ON e.%5$I = j.%5$I\n\t\tJOIN %I s ON e.%7$I = s.%7$I\n\tWHERE NOT e.%5$I = 0\n\tAND e.%7$I = %L\n';
+        const calcGrossQuery = format(calcGrossString, ...args);
 
-        dbQuery = format('INSERT INTO %I (%I)\nVALUES (%L)\nRETURNING *;', 'payroll', params.names, params.values);
-        const result = await utils.transacQuery(queries, client, dbQuery);
+        args = [
+          requiredFields,
+          'gross_income',
+          'tax_rate'
+        ];
+        const calcNet = format('%I,%I,%2$I * %I AS taxed_income,%2$I * (1 - %I) AS net_income', ...args);
+        args = [
+          'payroll',
+          [...requiredFields, 'gross_income', 'taxed_income', 'net_income'],
+          calcNet,
+          calcGrossQuery
+        ];
+        const dbQuery = format('INSERT INTO %I(%I)\nSELECT %s\nFROM (%s) AS gross_calc\nRETURNING *;', ...args);
+        const result = await client.query(dbQuery); 
 
         await utils.transacQuery(queries, client, 'COMMIT;');
         await utils.transacQuery(queries, client, 'END TRANSACTION;\n');
         client.release();
         res.json({
           rows: result.rows,
-          queries: queries,
-          transaction: true
-        });
-      } catch(err) {
-        console.log(err.stack);
-        await utils.transacQuery(queries, client, 'ROLLBACK;\n');
-        client.release();
-        res.status(422).json({
-          error: err.message,
-          queries: queries,
-          transaction: true
-        });
-      }
-    }
-  } else {
-    res.status(422).json({
-      error: 'Missing required fields',
-      requiredFields: requiredFields,
-      queries: [],
-      transaction: false
-    });
-  }
-});
-
-/**
- * @api {get} /payroll/:id Get pay details
- * @apiName GetPayroll
- * @apiGroup Payroll
- * @apiDescription Returns gross, taxed, and net income corresponding to a payroll entry. Returns no rows if the payroll ID does not exist.
- * 
- * @apiParam {Number} id Payroll ID
- * 
- * @apiSuccess {Object[]} rows                  Results from the database
- * @apiSuccess {Number}   rows.payroll_id       Payroll ID
- * @apiSuccess {Number}   rows.gross            Gross income
- * @apiSuccess {Number}   rows.tax              Taxed income
- * @apiSuccess {Number}   rows.net              Net income
- * 
- * @apiSuccess {String[]} queries               Array of queries used
- * @apiSuccess {Boolean}  transaction           True if transactions were used
- * 
- * @apiSuccessExample {json} Success-Response example:
- *    HTTP/1.1 200 OK
- *    {
- *      "rows": [{
- *                "payroll_id": 123,
- *                "gross": "$400",
- *                "tax": "$40",
- *                "net": "$360"
- *               }],
- *      "queries": ["SELECT *\nFROM table;"],
- *      "transaction": false
- *    }
- * 
- */
-router.get('/payroll/:id', async (req, res) => {
-  const id = req.params.id;
-  if(id && /^\d+$/.test(id)) {
-    let args = [
-      'hours_worked',
-      'weekly_hours',
-      'hourly_wage'
-    ];
-    const calcGross = format('\n\t\tCASE\n\t\t\tWHEN %I <= %I THEN %1$I * %3$I\n\t\t\tELSE (1.5 * %1$I - 0.5 * %I) * %I\n\t\tEND gross', ...args);
-    args = [
-      ['payroll_id', 'tax_rate'],
-      calcGross,
-      'payroll',
-      'job',
-      'job_id',
-      'salary',
-      'salary_id',
-      'payroll_id',
-      id
-    ];
-    const calcGrossString = '\n\tSELECT %I,%s\n\tFROM %I p\n\t\tJOIN %I j ON p.%5$I = j.%5$I\n\t\tJOIN %I s ON p.%7$I = s.%7$I\n\tWHERE %I = %L\n';
-    const calcGrossQuery = format(calcGrossString, ...args);
-
-    args = [
-      'payroll_id',
-      'gross',
-      'tax_rate',
-      calcGrossQuery
-    ];
-    const dbQuery = format('SELECT %I, %I, %2$I * %I AS tax, %2$I * (1 - %I) AS net\nFROM (%s) AS gross_calc;', ...args);
-    try {
-      const result = await db.query(dbQuery); 
-      res.json({
-        rows: result.rows, 
-        queries: [dbQuery],
-        transaction: false
-      });
-    } catch(err) {
-      console.error(err.stack);
-      res.status(422).json({
-        error: err.message,
-        queries: [dbQuery],
-        transaction: false
-      });
-    }
-  } else {
-    res.status(400).json({
-      error: 'Invalid or missing payroll id',
-      queries: [],
-      transaction: false
-    });
-  }
-});
-
-/**
- * @api {post} /payroll/all Create a new payroll entry for all employees
- * @apiName CreateAllPayrolls
- * @apiGroup Payroll
- * @apiDescription Attempts to insert a new payroll entry for every employee into the database.
- * 
- * @apiBody {String}   pay_period       Pay period
- * @apiBody {Number}   tax_rate         Tax rate
- * 
- * @apiSuccess {Number}   rowCount      Number of payrolls created
- * @apiSuccess {String[]} queries       Array of queries used
- * @apiSuccess {Boolean}  transaction   True if transactions were used
- * 
- * @apiSuccessExample {json} Success-Response example:
- *    HTTP/1.1 200 OK
- *    {
- *      "rowCount": 100,
- *      "queries": ["INSERT INTO table VALUES(...);"],
- *      "transaction": true
- *    }
- * 
- */
- router.post('/payroll/all', async (req, res) => {
-  // TODO: input validation
-  const body = req.body;
-  // Check required fields exist
-  const requiredFields = ['pay_period', 'tax_rate'];
-  if(utils.checkRequiredFields(requiredFields, body)) {
-    const client = await db.connect().catch((err) => {
-      console.log(err.stack);
-      res.status(422).json({
-        error: 'Error connecting to database',
-        queries: [],
-        transaction: false
-      });
-    });
-    if(client) {
-      let queries = [];
-      requiredFields.push('hours_worked');
-      body.hours_worked = 40;
-      try {
-        await utils.transacQuery(queries, client, 'BEGIN TRANSACTION;');
-        await utils.transacQuery(queries, client, 'SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;');
-
-        const empID = (await utils.transacQuery(queries, client, 'SELECT employee_ID\nFROM employee;')).rows;
-        for(const id of empID) {
-          let args = [
-            ['job_id', 'salary_id'], 
-            'employee', 
-            'salary', 
-            'leave', 
-            'employee_id',
-            id.employee_id
-          ];
-          let dbQuery = format('SELECT %I\nFROM %I e\n\tNATURAL JOIN %I\n\tLEFT JOIN %I l\n\tON l.%I = e.%5$I\nWHERE e.%5$I = %L;', ...args);
-          const ids = (await utils.transacQuery(queries, client, dbQuery)).rows[0];
-          
-          args = [
-            'payroll',
-            ['employee_id','pay_period','tax_rate','hours_worked','job_id','salary_id'],
-            [id.employee_id, body.pay_period, body.tax_rate, body.hours_worked, ids.job_id, ids.salary_id]
-          ];
-          dbQuery = format('INSERT INTO %I (%I)\nVALUES (%L);', ...args);
-          await utils.transacQuery(queries, client, dbQuery);
-        }
-        await utils.transacQuery(queries, client, 'COMMIT;');
-        await utils.transacQuery(queries, client, 'END TRANSACTION;\n');
-        client.release();
-        res.json({
-          rows: empID.length,
           queries: queries,
           transaction: true
         });
