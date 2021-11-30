5351b5e6d8fc3d4473498db40844c825a544aeee
nvalt-1 <78761232+nvalt-1@users.noreply.github.com>
2021-11-30 09:02:54 -0600

    WIP populated job table

--- create_table_script.sql
+++ create_table_script.sql
@@ -1,5 +1,16 @@
 -- Run these commands in ElephantSQL postgres database in browser tab to recreate starting tables for local testing
 
+DROP TABLE IF EXISTS payroll          CASCADE;
+DROP TABLE IF EXISTS benefits         CASCADE;
+DROP TABLE IF EXISTS salary           CASCADE;
+DROP TABLE IF EXISTS leave            CASCADE;
+DROP TABLE IF EXISTS department       CASCADE;
+DROP TABLE IF EXISTS job_location     CASCADE;
+DROP TABLE IF EXISTS job              CASCADE;
+DROP TABLE IF EXISTS employee         CASCADE;
+DROP TABLE IF EXISTS employee_address CASCADE;
+
+
 CREATE TABLE benefits (
   benefits_package_id        INT          GENERATED ALWAYS AS IDENTITY, 
   amount                     MONEY        NOT NULL,
@@ -42,7 +53,7 @@ CREATE TABLE department (
 
 CREATE TABLE job (
   job_id              INT         GENERATED ALWAYS AS IDENTITY, 
-  job_title           VARCHAR(30) NOT NULL CHECK(job_title ~ '^[A-Z \-]+$'),
+  job_title           VARCHAR(50) NOT NULL CHECK(job_title ~ '^[A-Z \-]+$'),
   department_id       INT         NOT NULL, 
   location_id         INT         NOT NULL,
   weekly_hours        INT         DEFAULT 40 CHECK(weekly_hours <= 168),
@@ -147,13 +158,14 @@ INSERT INTO job OVERRIDING SYSTEM VALUE VALUES(0, 'UNASSIGNED', 0, 0, 0, 0);
 INSERT INTO department (department_name, creation_date) VALUES 
 ('BOARD OF DIRECTORS',            '1980-01-01'),
 ('ENGINEERING AND MAINTENANCE',   '1980-01-01'),
-('OPERATIONS',                    '1980-01-01'),
+('GROUND OPERATIONS',             '1980-01-01'),
+('FLIGHT OPERATIONS',             '1980-01-01'),
 ('SALES AND MARKETING',           '1980-01-01'),
 ('RESERVATIONS AND TICKETING',    '1980-01-01'),
 ('FINANCE AND PROPERTY',          '1980-01-01'),
 ('INFORMATION SERVICES',          '1990-11-08'),
 ('PERSONNEL',                     '1980-01-01'),
-('MEDICAL',                       '1980-04-05'),
+('EMERGENCY SERVICES',            '1980-04-05'),
 ('LEGAL',                         '1981-03-11'),
 ('PUBLIC RELATIONS AND PLANNING', '1985-07-20');
 
@@ -202,7 +214,7 @@ INSERT INTO job_location (airport_id, address_id, flight_id, location_name) VALU
 
 -- Jobs (based on https://www.airlinejobfinder.com/members/airline-jobs/)
 INSERT INTO job (job_title, department_id, location_id, weekly_hours, benefits_package_id) VALUES
--- Board of Directors
+-- Board of Directors (1)
 ('CHIEF EXECUTIVE OFFICER', 1,  2,  50, 9),
 ('CHAIRMAN',                1,  2,  10, 9),
 ('VICE CHAIR',              1,  2,  10, 9),
@@ -210,7 +222,7 @@ INSERT INTO job (job_title, department_id, location_id, weekly_hours, benefits_p
 ('TREASURER',               1,  2,  10, 9),
 ('BOARD MEMBER',            1,  2,  5,  9),
 
--- Maintenance and Engineering
+-- Maintenance and Engineering (2)
 ('AVIATION MAINTENANCE TECHNICIAN', 2,  4,  40, 2),
 ('POWERPLANT MECHANIC',             2,  4,  40, 2),
 ('AIRFRAME MECHANIC',               2,  4,  40, 2),
@@ -255,5 +267,253 @@ INSERT INTO job (job_title, department_id, location_id, weekly_hours, benefits_p
 ('AIRPORT EQUIPMENT DRIVER',        2,  19,  40, 2),
 ('AIRPORT ENGINEER AND PLANNER',    2,  19,  40, 2),
 
--- Operations
-('BAGGAGE HANDLER', 3, 4, 40, 2),
+-- Ground Operations (3)
+('DIRECTOR OF AVIATION',        3, 1, 40, 3),
+('DEPUTY DIRECTOR OF AVIATION', 3, 1, 40, 3),
+('DIRECTOR OF AVIATION',        3, 2, 40, 3),
+('DEPUTY DIRECTOR OF AVIATION', 3, 2, 40, 3),
+('DIRECTOR OF AVIATION',        3, 3, 40, 3),
+('DEPUTY DIRECTOR OF AVIATION', 3, 3, 40, 3),
+
+('BAGGAGE HANDLER',               3,  4,  40, 5),
+('SKYCAP',                        3,  4,  40, 5),
+('CONCESSIONS',                   3,  4,  40, 5),
+('AIRPORT GUIDE',                 3,  4,  40, 5),
+('AIRPORT CUSTODIAN',             3,  4,  40, 1),
+('OPERATIONS AGENT',              3,  4,  40, 3),
+('AIRPORT STATION ATTENDANT',     3,  4,  40, 3),
+('PASSENGER SERVICE AGENT',       3,  4,  40, 3),
+('RAMP PLANNER',                  3,  4,  40, 3),
+('AIRLINE STATION AGENT',         3,  4,  40, 3),
+('TRANSPORTATION SECURITY AGENT', 3,  4,  40, 7),
+
+('BAGGAGE HANDLER',               3,  9,  40, 5),
+('SKYCAP',                        3,  9,  40, 5),
+('CONCESSIONS',                   3,  9,  40, 5),
+('AIRPORT GUIDE',                 3,  9,  40, 5),
+('AIRPORT CUSTODIAN',             3,  9,  40, 1),
+('OPERATIONS AGENT',              3,  9,  40, 3),
+('AIRPORT STATION ATTENDANT',     3,  9,  40, 3),
+('PASSENGER SERVICE AGENT',       3,  9,  40, 3),
+('RAMP PLANNER',                  3,  9,  40, 3),
+('AIRLINE STATION AGENT',         3,  9,  40, 3),
+('TRANSPORTATION SECURITY AGENT', 3,  9,  40, 7),
+
+('BAGGAGE HANDLER',               3,  14, 40, 5),
+('SKYCAP',                        3,  14, 40, 5),
+('CONCESSIONS',                   3,  14, 40, 5),
+('AIRPORT GUIDE',                 3,  14, 40, 5),
+('AIRPORT CUSTODIAN',             3,  14, 40, 1),
+('OPERATIONS AGENT',              3,  14, 40, 3),
+('AIRPORT STATION ATTENDANT',     3,  14, 40, 3),
+('PASSENGER SERVICE AGENT',       3,  14, 40, 3),
+('RAMP PLANNER',                  3,  14, 40, 3),
+('AIRLINE STATION AGENT',         3,  14, 40, 3),
+('TRANSPORTATION SECURITY AGENT', 3,  14, 40, 7),
+
+('BAGGAGE HANDLER',               3,  19, 40, 5),
+('SKYCAP',                        3,  19, 40, 5),
+('CONCESSIONS',                   3,  19, 40, 5),
+('AIRPORT GUIDE',                 3,  19, 40, 5),
+('AIRPORT CUSTODIAN',             3,  19, 40, 1),
+('OPERATIONS AGENT',              3,  19, 40, 3),
+('AIRPORT STATION ATTENDANT',     3,  19, 40, 3),
+('PASSENGER SERVICE AGENT',       3,  19, 40, 3),
+('RAMP PLANNER',                  3,  19, 40, 3),
+('AIRLINE STATION AGENT',         3,  19, 40, 3),
+('TRANSPORTATION SECURITY AGENT', 3,  19, 40, 7),
+
+-- Flight Operations (4)
+('FLIGHT DISPATCHER',       3,  4,  40, 6),
+('AVIATION METEOROLOGIST',  3,  4,  40, 6),
+('FLIGHT DISPATCHER',       3,  9,  40, 6),
+('AVIATION METEOROLOGIST',  3,  9,  40, 6),
+('FLIGHT DISPATCHER',       3,  14, 40, 6),
+('AVIATION METEOROLOGIST',  3,  14, 40, 6),
+('FLIGHT DISPATCHER',       3,  19, 40, 6),
+('AVIATION METEOROLOGIST',  3,  19, 40, 6),
+
+('CARGO CARRIER PILOT', 4,  5,  60, 6),
+('COPORATE PILOT',      4,  5,  30, 6),
+('CHARTER PILOT',       4,  5,  60, 6),
+
+('CARGO CARRIER PILOT', 4,  6,  60, 6),
+('COPORATE PILOT',      4,  6,  30, 6),
+('CHARTER PILOT',       4,  6,  60, 6),
+
+('CARGO CARRIER PILOT', 4,  7,  60, 6),
+('COPORATE PILOT',      4,  7,  30, 6),
+('CHARTER PILOT',       4,  7,  60, 6),
+
+('CARGO CARRIER PILOT', 4,  8,  60, 6),
+('COPORATE PILOT',      4,  8,  30, 6),
+('CHARTER PILOT',       4,  8,  60, 6),
+
+('CARGO CARRIER PILOT', 4,  10, 60, 6),
+('COPORATE PILOT',      4,  10, 30, 6),
+('CHARTER PILOT',       4,  10, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  11, 60, 6),
+('COPORATE PILOT',      4,  11, 30, 6),
+('CHARTER PILOT',       4,  11, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  12, 60, 6),
+('COPORATE PILOT',      4,  12, 30, 6),
+('CHARTER PILOT',       4,  12, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  13, 60, 6),
+('COPORATE PILOT',      4,  13, 30, 6),
+('CHARTER PILOT',       4,  13, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  15, 60, 6),
+('COPORATE PILOT',      4,  15, 30, 6),
+('CHARTER PILOT',       4,  15, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  16, 60, 6),
+('COPORATE PILOT',      4,  16, 30, 6),
+('CHARTER PILOT',       4,  16, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  17, 60, 6),
+('COPORATE PILOT',      4,  17, 30, 6),
+('CHARTER PILOT',       4,  17, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  18, 60, 6),
+('COPORATE PILOT',      4,  18, 30, 6),
+('CHARTER PILOT',       4,  18, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  20, 60, 6),
+('COPORATE PILOT',      4,  20, 30, 6),
+('CHARTER PILOT',       4,  20, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  21, 60, 6),
+('COPORATE PILOT',      4,  21, 30, 6),
+('CHARTER PILOT',       4,  21, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  22, 60, 6),
+('COPORATE PILOT',      4,  22, 30, 6),
+('CHARTER PILOT',       4,  22, 60, 6),
+
+('CARGO CARRIER PILOT', 4,  23, 60, 6),
+('COPORATE PILOT',      4,  23, 30, 6),
+('CHARTER PILOT',       4,  23, 60, 6),
+
+-- Sales and Marketing (5)
+('REGIONAL SALES MANAGER',  5,  1,  40, 3),
+('SALES REPRESENTATIVE',    5,  1,  40, 3),
+('AIRLINE MARKETER',        5,  1,  40, 3),
+('SALES INTERN',            5,  1,  20, 3),
+
+('REGIONAL SALES MANAGER',  5,  2,  40, 3),
+('SALES REPRESENTATIVE',    5,  2,  40, 3),
+('AIRLINE MARKETER',        5,  2,  40, 3),
+('SALES INTERN',            5,  2,  20, 3),
+
+('REGIONAL SALES MANAGER',  5,  3,  40, 3),
+('SALES REPRESENTATIVE',    5,  3,  40, 3),
+('AIRLINE MARKETER',        5,  3,  40, 3),
+('SALES INTERN',            5,  3,  20, 3),
+
+-- Reservations and Ticketing (6)
+('RESERVATION SALES AGENT', 6,  4,  40, 3),
+('AIRLINE TICKET AGENT',    6,  4,  40, 3),
+
+('RESERVATION SALES AGENT', 6,  9,  40, 3),
+('AIRLINE TICKET AGENT',    6,  9,  40, 3),
+
+('RESERVATION SALES AGENT', 6,  14, 40, 3),
+('AIRLINE TICKET AGENT',    6,  14, 40, 3),
+
+('RESERVATION SALES AGENT', 6,  19, 40, 3),
+('AIRLINE TICKET AGENT',    6,  19, 40, 3),
+
+-- Finance and Property (7)
+('ACCOUNTANT',                7,  1,  40, 5),
+('AUDITOR',                   7,  1,  40, 5),
+('FINANCE ASSITANT',          7,  1,  40, 5),
+('JUNIOR FINANCIAL ANALYST',  7,  1,  40, 5),
+('PAYROLL CLERK',             7,  1,  40, 5),
+('FINANCIAL ANALYST',         7,  1,  40, 5),
+('PAYROLL MANAGER',           7,  1,  40, 5),
+('FINANCE INTERN',            7,  1,  20, 5),
+
+-- Information services (8)
+('IT SPECIALIST',     8,  1,  40, 3),
+('IT INTERN',         8,  1,  20, 3),
+('HELP DESK SUPPORT', 8,  1,  40, 3),
+
+('IT SPECIALIST',     8,  2,  40, 3),
+('HELP DESK SUPPORT', 8,  2,  40, 3),
+('IT INTERN',         8,  2,  20, 3),
+
+('IT SPECIALIST',     8,  3,  40, 3),
+('HELP DESK SUPPORT', 8,  3,  40, 3),
+('IT INTERN',         8,  3,  20, 3),
+
+-- Personnel (9)
+('SECRETARY',                   9,  1,  40, 3),
+('MAIL CLERK',                  9,  1,  40, 3),
+('HUMAN RESOURCES GENERALIST',  9,  1,  40, 3),
+('HUMAN RESOURCES MANAGER',     9,  1,  40, 3),
+('RECEPTIONIST',                9,  1,  40, 3),
+('CREW SCHEDULE COORDINATOR',   9,  1,  40, 3),
+
+('SECRETARY',                   9,  2,  40, 3),
+('MAIL CLERK',                  9,  2,  40, 3),
+('HUMAN RESOURCES GENERALIST',  9,  2,  40, 3),
+('HUMAN RESOURCES MANAGER',     9,  2,  40, 3),
+('RECEPTIONIST',                9,  2,  40, 3),
+('CREW SCHEDULE COORDINATOR',   9,  2,  40, 3),
+
+('SECRETARY',                   9,  3,  40, 3),
+('MAIL CLERK',                  9,  3,  40, 3),
+('HUMAN RESOURCES GENERALIST',  9,  3,  40, 3),
+('HUMAN RESOURCES MANAGER',     9,  3,  40, 3),
+('RECEPTIONIST',                9,  3,  40, 3),
+('CREW SCHEDULE COORDINATOR',   9,  3,  40, 3),
+
+-- Emergency Services (10)
+('FIREFIGHTER',           10,  4,  40, 10),
+('FIREFIGHTER MEDIC',     10,  4,  40, 10),
+('POLICE OFFICER',        10,  4,  40, 10),
+('FIRE AND CRASH RESCUE', 10,  4,  30, 10),
+('POLICE PATROL OFFICER', 10,  4,  30, 10),
+
+('FIREFIGHTER',           10,  9,  40, 10),
+('FIREFIGHTER MEDIC',     10,  9,  40, 10),
+('POLICE OFFICER',        10,  9,  40, 10),
+('FIRE AND CRASH RESCUE', 10,  9,  30, 10),
+('POLICE PATROL OFFICER', 10,  9,  30, 10),
+
+('FIREFIGHTER',           10,  14, 40, 10),
+('FIREFIGHTER MEDIC',     10,  14, 40, 10),
+('POLICE OFFICER',        10,  14, 40, 10),
+('FIRE AND CRASH RESCUE', 10,  14, 30, 10),
+('POLICE PATROL OFFICER', 10,  14, 30, 10),
+
+('FIREFIGHTER',           10,  19, 40, 10),
+('FIREFIGHTER MEDIC',     10,  19, 40, 10),
+('POLICE OFFICER',        10,  19, 40, 10),
+('FIRE AND CRASH RESCUE', 10,  19, 30, 10),
+('POLICE PATROL OFFICER', 10,  19, 30, 10),
+
+-- Legal (11)
+('AVIATION ATTORNEY',  11,  1,  40, 7),
+('AVIATION ATTORNEY',  11,  2,  40, 7),
+('AVIATION ATTORNEY',  11,  3,  40, 7),
+
+-- Public Relations and Planning (12)
+('PUBLIC RELATIONS SPECIALIST', 12,  1,  40, 3),
+('PUBLIC RELATIONS GENERALIST', 12,  1,  40, 3),
+('PUBLIC RELATIONS MANAGER',    12,  1,  40, 3),
+('PUBLIC RELATIONS INTERN',     12,  1,  20, 3),
+
+('PUBLIC RELATIONS SPECIALIST', 12,  2,  40, 3),
+('PUBLIC RELATIONS GENERALIST', 12,  2,  40, 3),
+('PUBLIC RELATIONS MANAGER',    12,  2,  40, 3),
+('PUBLIC RELATIONS INTERN',     12,  2,  20, 3),
+
+('PUBLIC RELATIONS SPECIALIST', 12,  3,  40, 3),
+('PUBLIC RELATIONS GENERALIST', 12,  3,  40, 3),
+('PUBLIC RELATIONS MANAGER',    12,  3,  40, 3),
+('PUBLIC RELATIONS INTERN',     12,  3,  20, 3);
+
